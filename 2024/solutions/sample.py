fd5ad76a31f942fc2b092719e800f6887a753683
aspedrosa <aspedr0sa@protonmail.com>
2024-12-04 20:02:08 +0000

    sample file for new solutions file

---
+++ 2024/solutions/sample.py
@@ -0,0 +1,17 @@
+
+def _parse_input(lines):
+    return lines
+
+
+def puzzle1(lines):
+    lines = _parse_input(lines)
+
+    #
+    return
+
+
+def puzzle2(lines):
+    lines = _parse_input(lines)
+
+    #
+    return
