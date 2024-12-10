56b15b6e7007656af06845bdd46e0c5bfdfd2877
aspedrosa <aspedr0sa@protonmail.com>
2024-12-10 20:29:56 +0000

    also add zeros to solutions + adapt main to support zeros

--- 2024/main.py
+++ 2024/main.py
@@ -1,18 +1,18 @@
 import importlib
 import os.path
 
-SAMPLE = 0
-DAY = 5
-PUZZLE = 2
+SAMPLE = 1
+DAY = 10
+PUZZLE = 1
 
 
 def main():
     directory = "samples" if SAMPLE else "inputs"
     try:
-        f = open(os.path.join(directory, f"day{DAY}.txt"))
+        f = open(os.path.join(directory, f"day{DAY:0>2}.txt"))
     except FileNotFoundError:
         if SAMPLE:
-            f = open(os.path.join(directory, f"day{DAY}_puzzle{PUZZLE}.txt"))
+            f = open(os.path.join(directory, f"day{DAY:0>2}_puzzle{PUZZLE}.txt"))
         else:
             raise
 
@@ -21,7 +21,7 @@ def main():
         lines = [l.strip() for l in lines]
         #lines = [l for l in lines if l]
 
-    solution_module = importlib.import_module(f"solutions.day{DAY}")
+    solution_module = importlib.import_module(f"solutions.day{DAY:0>2}")
     solution_function = getattr(solution_module, f"puzzle{PUZZLE}")
 
     solution = solution_function(lines)
